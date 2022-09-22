445fe1494897164a076931d01e25f2ed592c24ce
nahim888 <ahmed.nahim@iisgalvanimi.edu.it>
2022-09-22 09:31:53 +0000

    punto 2

--- ahmed/src/app/app.module.ts
+++ ahmed/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SecondoComponent } from './secondo/secondo.component';
+import { TerzoComponent } from './terzo/terzo.component';
+import { QuartoComponent } from './quarto/quarto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondoComponent,
+    TerzoComponent,
+    QuartoComponent
   ],
   imports: [
     BrowserModule
---
+++ ahmed/src/app/terzo/terzo.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TerzoComponent } from './terzo.component';
+
+describe('TerzoComponent', () => {
+  let component: TerzoComponent;
+  let fixture: ComponentFixture<TerzoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TerzoComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TerzoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
